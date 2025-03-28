0e213fb7ceb5971dab995dc02d61911b59cf9baf
Pedro95DH <pedrosdhdez@gmail.com>
2025-03-28 11:38:18 +0000

    .

--- my-portfolio/components/Recomendations/recomend.js
+++ my-portfolio/components/Recomendations/recomend.js
@@ -2,6 +2,6 @@ import "./recomend.css";
 
 export const recomendation = (recomendaciones) =>
   `
-<a class="recomendLink" href=${recomendaciones.link}> <img src=${recomendaciones.icon}/> <p>${recomendaciones.title}` +
+<a class="recomendLink" href=${recomendaciones.link}> <img src=${recomendaciones.icon} alt=${recomendaciones.title}/> <p>${recomendaciones.title}` +
   ` - ` +
   `${recomendaciones.description}</p></a>`;
