57db84e187345740999871fa4a2cca91e08d317e
Pedro95DH <pedrosdhdez@gmail.com>
2025-03-28 12:29:52 +0000

    .

--- my-portfolio/data/projects.js
+++ my-portfolio/data/projects.js
@@ -1,10 +1,10 @@
 export const projects = [
   {
-    title: "Arquitectura HTML",
+    title: "Arquitectura html",
     description: "Este ejercicio se enfoca en la arquitectura de html y en el uso de buenas prácticas a la hora de usar las etiquetas correctamente.",
     image:
       "/images/arquitecturahtml.webp",
-    tech: ["HTML5"],
+    tech: ["html5"],
     github: "http://https://github.com/Pedro95DH/html5_arquitectura_informacion",
     link: "https://pedro95dh.github.io/html5_arquitectura_informacion/",
   },
@@ -13,7 +13,7 @@ export const projects = [
     description: "Aquí me centro en entender los diferentes métodos de posicionamiento básicos que hay en css ( absolute, relative, etc...)",
     image:
       "/images/posicionamiento.webp",
-    tech: ["HTML5", "CSS3"],
+    tech: ["html5", "css3"],
     github: "http://github.com/Pedro95DH/Entrega_Posicionamiento",
     link: "https://pedro95dh.github.io/Entrega_Posicionamiento/",
   },
@@ -22,7 +22,7 @@ export const projects = [
     description: "Puesta en práctica del posicionamiento flex.",
     image:
       "/images/flex.webp",
-    tech: ["HTML5", "CSS3"],
+    tech: ["html5", "css3"],
     github: "https://github.com/Pedro95DH/Entrega_Flexbox",
     link: "https://pedro95dh.github.io/Entrega_Flexbox/",
   },
@@ -31,7 +31,7 @@ export const projects = [
     description: "Primeros pasos con el posicionamiento grid.",
     image:
       "/images/grid.webp",
-    tech: ["HTML5", "CSS3"],
+    tech: ["html5", "css3"],
     github: "https://github.com/Pedro95DH/Entrega_Grid",
     link: "https://pedro95dh.github.io/Entrega_Grid/",
   },
@@ -40,7 +40,7 @@ export const projects = [
     description: "Primer proyecto en el que aplico todo lo aprendido y replico una landing page real.",
     image:
       "/images/proyecto1.webp",
-    tech: ["HTML5", "CSS3"],
+    tech: ["html5", "css3"],
     github: "https://github.com/Pedro95DH/Entrega_Proyecto1",
     link: "https://pedro95dh.github.io/Entrega_Proyecto1/",
   },
@@ -49,7 +49,7 @@ export const projects = [
     description: "Este proyecto usa de referencia el anterior, pero ahora usando javascript para la creación de las cuadrículas de productos, opiniones y añadiendo un menú desplegable. Así pongo en práctica Javascript.",
     image:
       "/images/dom.webp",
-    tech: ["HTML5", "CSS3", "Javascript"],
+    tech: ["html5", "css3", "javascript"],
     github: "https://github.com/Pedro95DH/Entrega_practica_DOM",
     link: "https://pedro95dh.github.io/Entrega_practica_DOM/",
   },
