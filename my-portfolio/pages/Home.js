9a6a831de10f876b53f0780a3503e3e4dfafe63d
Pedro95DH <pedrosdhdez@gmail.com>
2025-03-31 23:01:23 +0100

    .

--- my-portfolio/pages/Home.js
+++ my-portfolio/pages/Home.js
@@ -9,7 +9,7 @@ export const Home = () => {
     <p>Hola, soy</p>
     <h1>Pedro Díaz</h1>
     <p>Soy un estudiante del Máster de Desarrollo Full Stack en The Power. Estoy comenzando en el mundo de la programación y me apasiona aprender como funcionan los lenguajes y como se refleja en las aplicaciones finales,
-    como ser convierten en algo útil en el mundo y sirven para resolver las necesidades de los que quieren tener una aplicación web para resolver sus necesidades.</p>
+    como ser convierten en algo útil en el mundo y sirven para resolver las necesidades de los que quieren tener una aplicación web.</p>
     <a href="mailto:pedrosdhdez@gmail.com">¡Di hola! →</a>
     </section>`;
 };
