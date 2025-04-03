321cf5e7cb058c3a390039fcbb2352a47814aee8
Pedro95DH <pedrosdhdez@gmail.com>
2025-04-03 20:02:20 +0100

    some comiits
    .

--- my-portfolio/components/Technologies/technologies.js
+++ my-portfolio/components/Technologies/technologies.js
@@ -1,5 +1,7 @@
 import "./technologies.css";
 
+//Modifique como se mostraban las tecnologias en las projectscards, ahora en vez de salir el texto aparece la imagen representativa de cada tecnologia.
+
 export const techies = (project) => {
   const p = document.createElement("p");
   project.tech.forEach((tech) => {
--- my-portfolio/pages/Recomendations.js
+++ my-portfolio/pages/Recomendations.js
@@ -4,6 +4,8 @@ import { Divider } from "../components/Divider/Divider";
 import { recomendation } from "../components/Recomendations/recomend";
 import { recomendations } from "../data/recomendations";
 
+//Genero la sección de recomendaciones y controlo si algún icono es en blanco y negro teniendo un atributo con el campo blackwhite asociado del array de objetos que contiene los datos de recomendations.js para crear esta seccion, asi con el cambio del tema de página solo se cambian estos iconos indicados.
+
 export const Recomendations = () => {
   const main = document.querySelector("main");
   cleanPage(main);
